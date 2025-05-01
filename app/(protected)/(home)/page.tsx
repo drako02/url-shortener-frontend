4646bf7389d5c9cd9031792cb9d6a10b3fc21594
drako02 <andrewappah.aa@gmail.com>
2025-05-01 21:31:04 +0000

    Remove unused HomeScreen and UserUrlsPage components

--- app/(protected)/(home)/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// import { UserHomepage } from "@/mycomponents/pages/home/page";
-// import { useAuth } from "../../context/Auth";
-
-export default function HomeScreen() {
-  return <></>;
-}
