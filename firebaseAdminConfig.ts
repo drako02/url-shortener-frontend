df6701568816f71876b2a7fc80cb2feeb61f8bc3
drako02 <andrewappah.aa@gmail.com>
2025-05-01 19:45:49 +0000

    Refactor Firebase Admin configuration to use environment variable for service account and improve error handling

--- firebaseAdminConfig.ts
+++ firebaseAdminConfig.ts
@@ -4,16 +4,19 @@ import {
   cert,
   ServiceAccount,
 } from "firebase-admin/app";
-import serviceAccountJSON from "./.cred/url-shortner-784fc-firebase-adminsdk-fbsvc-c7b0277b29.json";
+// import serviceAccountJSON from "./.cred/url-shortner-784fc-firebase-adminsdk-fbsvc-c7b0277b29.json";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  : serviceAccountJSON;
+const serviceAccountVar = process.env.FIREBASE_SERVICE_ACCOUNT
+console.log()
+  // ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  // : serviceAccountJSON;
 
-if (!serviceAccount) {
+if (!serviceAccountVar) {
   throw new Error("Missing FIREBASE_SERVICE_ACCOUNT environment variable");
 }
+
+const serviceAccount = JSON.parse(serviceAccountVar)
 
 const adminApp =
   getApps().length === 0
